8642d8b62827d131c1d3b0f58f379b44af501e24
bhimlex13 <oroalexander004@gmail.com>
2025-08-14 15:16:58 +0900

    First Commit

---
+++ Sample/sample.js
@@ -0,0 +1,45 @@
+// This is the data for the quiz questions, answers, and choices
+var quizData = [
+    {
+        "question": "Types of communication channel that use optic cable or light pipe that carries the message on a light wave.",
+        "image": "./download.jpg",
+        "answer": "Optical Media",
+        "choices": [
+            "Electrical Conductor",
+            "Optical Media",
+            "Sonars",
+            "Wireless Media"
+        ]
+    },
+    {
+        "question": "What way of communication shown above?",
+        "answer": "Simplex Communication",
+        "choices": [
+            "Duplex Communication",
+            "Half Duplex Communication",
+            "Simplex Communication",
+            "Analog Communication"
+        ]
+    },
+    {
+        "question": "It is a set of electronic components and circuits that accept transmitted messages from the channel and translate them back, which humans understand.",
+        "answer": "Receiver",
+        "choices": [
+            "Transmitter",
+            "Receiver",
+            "Amplifier",
+            "Decoder"
+        ]
+    },
+    {
+        "question": "Type of receiver where resonant circuits, combine an inductor and capacitor to make a circuit responsive to a frequency.",
+        "answer": "Tuned Circuits",
+        "choices": [
+            "Amplifiers",
+            "Tuned Circuits",
+            "Filters",
+            "Oscillators"
+        ]
+    }
+];
+
